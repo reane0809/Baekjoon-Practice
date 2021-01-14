26e46fe186a23a59e848b6c910e7936bcdb7aeed
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-15 07:55:58 +0900

    Create 10871.c

---
+++ 10871.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int n, x;
+  int a[10000],b[10000];
+  int i=0;
+  scanf("%d %d",&n,&x);
+  for(int j=0;j<n;j++){
+    scanf("%d",&a[j]);
+    if(a[j]<x){
+      b[i] = a[j];
+      i++;
+    }
+  }
+  for(int k=0;k<i;k++){
+    if(k!=i-1){
+      printf("%d ",b[k]);
+    }else printf("%d",b[i-1]);
+  }
+}
