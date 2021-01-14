bebb6a87d7693da22e278d7b175bfb178a4623d5
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-14 09:03:42 +0900

    Create 1330.c

---
+++ 1330.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,i;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    if(a>b)
+        printf(">");
+    else if(a<b)
+        printf("<");
+    else if(a==b)
+        printf("==");
+    
+    return 0;
+}
