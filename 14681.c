ab573ec734af9a8048ecdba7836d069e781a1d72
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-16 08:30:10 +0900

    Create 14681.c

---
+++ 14681.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int x,y;
+    scanf("%d\n", &x);
+    scanf("%d\n", &y);
+    
+    if(x>0 && y>0)
+        printf("1");
+    else if(x<0 && y>0)
+        printf("2");
+    else if(x<0 && y<0)
+        printf("3");
+    else if(x>0 && y<0)
+        printf("4");
+    
+   return 0;
+}
