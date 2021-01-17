6259138f75f25b58168aa055a4a1a220e07d10c5
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-18 01:45:27 +0900

    Create 1110.py

---
+++ 1110.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = a
+count = 0
+while True:
+    if b == a%10 * 10 + (a//10 + a%10)%10:
+        count = count + 1
+        print(count)
+        break
+    else:
+        a = a%10 * 10 + (a//10 + a%10)%10
+        count = count + 1
