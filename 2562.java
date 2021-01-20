3c9837e571835948202d7d45716399471caaed13
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-21 05:00:57 +0900

    Create 2562.java

---
+++ 2562.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int num = 0;
+    	int i = 1;
+    	int place = 0;
+    	while (i <= 9) {
+    		int newNum = sc.nextInt();
+    		if (newNum > num) {
+    			num = newNum;
+    			place = i;
+    		}
+                		i++;
+        }
+    	System.out.println(num);
+    	System.out.print(place);
+    }
+}
