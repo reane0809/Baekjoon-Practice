d05063455c5847c162c015e46a7dfdea255457e7
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-26 05:13:21 +0900

    Create 11720.c

---
+++ 11720.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int count;
+	char numarr[101];
+	int sum = 0;
+	int buf = 0;
+	scanf("%d", &count);
+	
+	scanf("%s", numarr);
+
+	for (int i = 0; i < count; i++)
+	{
+		buf = numarr[i] - '0'; // 이 부분을 buf = atoi(numarr[i]);로 하니깐 안됨
+		sum += buf; 
+	}
+
+	printf("%d", sum);
+	
+
+	return 0;
+}
