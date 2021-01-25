de6ac2ef4512b44a57c3e2e2a3036fe76e3bd3b1
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-26 05:13:02 +0900

    Create 11720.java

---
+++ 11720.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int sum = 0;
+		String num = scan.next();
+		
+		for(int i=0; i<n; i++) {
+			String arr[] = num.split("");
+//			System.out.println(arr[i]);
+			int num2 = Integer.parseInt(arr[i]);
+			
+			sum += num2;
+		}
+		System.out.print(sum);
+		scan.close();
+	}
+}
