7fd685a855539697d4728ead4e033536dc1e8f93
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-27 08:10:02 +0900

    Create 10809.c

---
+++ 10809.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+
+	char S[101] = {NULL};
+	int count[26] = {0,};
+	scanf("%s",&S);
+	for(int i='a';i<='z';i++) {
+  
+		for(int j =0;j<100;j++){
+			if(S[j] == i) {
+				count[i-97] = j+1;
+				break;
+			}
+			
+		}
+	}
+	for(int i=0;i<26;i++) {
+		printf("%d ",count[i]-1);
+	}
+}
