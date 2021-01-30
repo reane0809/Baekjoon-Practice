ef9f9fe76ee8640f7db7e79ff3922771148fa1b0
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-01-30 09:04:32 +0900

    Create 1152(jdk11).java

---
+++ 1152(jdk11).java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String[] words = s.split(" ");
+        int count = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (!words[i].isBlank()) {
+                count++;
+            }
+
+        }
+
+        System.out.println(count);
+
+
+    }
+}
