3c8d37c76d2a8993afe0f787d20d9a6616c4076f
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-02 05:52:46 +0900

    Create 1316.java

---
+++ 1316.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		
+		for(int i=0;i<N;i++) {
+			String word = br.readLine();
+			boolean checker = true;
+			HashSet<Character> set = new HashSet();
+			char prev = word.charAt(0);
+			
+			for(int j =1;j<word.length();j++) {
+				char curr = word.charAt(j);
+				if(set.contains(curr)) {
+					checker = false;
+					break;
+				}
+				if(curr != prev) {
+					set.add(prev);
+					prev = curr;
+				}
+			}
+			if(checker) count++;
+		}
+		
+		System.out.println(count);
+	}
+
+}
