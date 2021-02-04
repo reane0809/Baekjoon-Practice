756bb2d1c73cd97e0b74d79876730c0b281dad4d
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-05 05:03:07 +0900

    Create 1193.py

---
+++ 1193.py
@@ -0,0 +1,16 @@
+num = int(input())
+n = 1
+ans = 0
+while(1):
+    if num == 1:
+        print(str(1) + '/' + str(1))
+        break
+    if n*(n+1)/2 >= num:
+        ans = num - (n-1)*n/2
+        if n%2==0:
+            print(str(int(ans)) + '/' + str(int(n-ans+1)))
+            break
+        else:
+            print(str(int(n-ans+1)) + '/' + str(int(ans)))
+            break
+    n += 1
