102297a9f02076edde85ea652430ed373a2b8b65
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-07 00:07:51 +0900

    Create 10250.java

---
+++ 10250.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String str = bf.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            if (N%H != 0) {
+                int hosu = ((N / H) + 1);
+                int count = (N % H) * 100;
+                System.out.println(hosu + count);
+            }else{
+               int hosu = (N/H);
+               int count = H*100;
+                System.out.println(hosu+count);
+            }
+        }
+    }
+}
