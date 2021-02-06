6b2caceeeee0cdd5714f917425a58ad97125e797
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-07 00:07:13 +0900

    Create 10250.py

---
+++ 10250.py
@@ -0,0 +1,11 @@
+import sys
+
+T = int(sys.stdin.readline().strip())
+
+for _ in range(T):
+    # H,W,N 입력 받기
+    H, W, N = map(int, sys.stdin.readline().split())
+    # cont : 호수, a : 층
+    cont = (N+H-1)//H
+    a = N % H if N % H != 0 else H
+    print("%d%02d" % (a, cont))
