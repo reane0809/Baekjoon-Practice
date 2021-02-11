a6a7388daeed4fa30eaba16261e25ec5b78a28d0
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-12 05:10:29 +0900

    Create 1978.py

---
+++ 1978.py
@@ -0,0 +1,23 @@
+import sys
+
+test_case = int(sys.stdin.readline())
+num_list = list(map(int, input().split()))
+count = 0
+
+
+def demical_test(a):
+    if a < 2:
+        return False
+
+    for j in range(2, a):
+        if a % j == 0:
+            return False
+
+    return True
+
+
+for i in range(len(num_list)):
+    if demical_test(num_list[i]):
+        count += 1
+
+print(count)
