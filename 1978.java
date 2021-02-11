45a00603c6106c5aa6c0b7eff003ae0fa412ee36
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-12 05:10:46 +0900

    Create 1978.java

---
+++ 1978.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        boolean temp = true;
+        int cnt = 0;
+        for(int j = 0; j < N; j++ ){
+            int M = Integer.parseInt(st.nextToken());
+            if(M != 1){
+                for(int i = 2; i <= Math.sqrt(M) ; i++){
+                    if(M % i == 0){
+                        temp = false;
+                        break;
+                    }
+                }
+
+            }else{
+                temp = false;
+            }
+            if(temp == true){
+                cnt += 1;
+            }
+            temp = true;
+        }
+        System.out.println(cnt);
+    }
+}
