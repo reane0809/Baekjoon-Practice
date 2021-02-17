5c2b53086cec7643390ec2da4d543a7c0e56156e
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-18 01:44:00 +0900

    Create 1085.java

---
+++ 1085.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		System.out.println(Math.min(Math.min(w-x, x), Math.min(h-y, y)));
+	}
+}
