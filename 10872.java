ba522bc92df289137322d78259710bbe6b481cf3
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-02-23 02:39:00 +0900

    Create 10872.java

---
+++ 10872.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int input = Integer.parseInt(br.readLine());
+		System.out.println(factorial(input));
+	}
+ 
+	public static int factorial(int n) {
+		if(n == 1 || n == 0) {
+	        return 1;
+	    }
+	    return n * factorial(n-1);
+	}
+	
+}
