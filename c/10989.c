91ca38652df25b660a0bc83f169c66656b53eb35
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-03-06 01:31:03 +0900

    Create 10989.c

---
+++ c/10989.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number[10000] = {0};
+    int num, n, count = 0;
+    scanf("%d", &num);
+    for(int i = 0; i < num; i++) {
+        scanf("%d", &n);
+        number[n - 1]++;
+    }
+    for(int i = 0; i < 10000; i++) {
+        for(int j = 0; j < number[i]; j++) {
+            count++;
+            printf("%d\n", i + 1);
+            if(count == num)
+                return 0;
+        }
+    }
+
+    return 0;
+}
