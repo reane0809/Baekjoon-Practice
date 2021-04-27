dd5f866e17e2895d92231e9cf343e4d0e05536ad
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-04-28 00:34:34 +0900

    Create 1427.java

---
+++ 1427.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine();
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = 0;i<n.length();i++) {
+			list.add(Character.getNumericValue(n.charAt(i)));
+		}
+		Collections.sort(list);
+		Collections.reverse(list);
+		for(int i = 0;i<list.size();i++) {
+			System.out.print(list.get(i));
+		}
+	}
+
+}
