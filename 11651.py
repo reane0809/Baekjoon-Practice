a15e828905dec60a7ede9cd5052a0795e421aca6
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-04-30 02:20:52 +0900

    Create 11651.py

---
+++ 11651.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+coords = [input() for _ in range(int(input()))]
+coords = sorted(coords, key = lambda coord: (int(coord.split()[1]), int(coord.split()[0])))
+
+print(''.join(coords))
