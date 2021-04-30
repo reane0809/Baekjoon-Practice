b849d4ca1be9691560999b4b28e3bc540083aa77
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-05-01 00:59:51 +0900

    Create 1181.py

---
+++ 1181.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline())
+arr = []
+for i in range(n):
+  arr.append(sys.stdin.readline().strip())
+arr = list(set(arr))
+arr2 = sorted(sorted(arr), key = lambda x: len(x))
+l = len(arr2)
+print('\n'.join(arr2))
