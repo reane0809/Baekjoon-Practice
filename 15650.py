d996bff9b3d9147b873c0bf3ad024bbdfebd49f2
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-05-12 01:38:56 +0900

    Create 15650.py

---
+++ 15650.py
@@ -0,0 +1,23 @@
+import sys
+
+#sys.stdin = open('input.txt','r')
+
+
+N,M = map(int,sys.stdin.readline().rstrip().split())
+
+def dfs(List,index):
+    if len(List) == M:
+        for n,j in enumerate(List):
+            if n < len(List) - 1:
+                print(j,end=" ")
+            else:
+                print(j)
+
+    for i in range(index,N+1):
+        if i not in List:
+            List.append(i)
+            dfs(List,i+1)
+            List.pop()
+
+List = []
+dfs(List, 1)
