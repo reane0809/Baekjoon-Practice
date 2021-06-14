56d9799bdab6efa6e76548c3258cc8a70267c283
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-06-15 00:14:05 +0900

    Create 1463.py

---
+++ 1463.py
@@ -0,0 +1,17 @@
+N=int(input())
+s=set()
+r=set()
+result=0
+s.add(N)
+while 1 not in s:
+	result+=1
+	k=s.copy()
+	for i in k:
+		if i%3==0:
+			s.add(i//3)
+		if i%2==0:
+			s.add(i//2)
+		s.add(i-1)
+		r.add(i)
+	s=s-r
+print(result)
