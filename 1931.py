83696bb2dca55773cea0c57d5422889acecc223c
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-07-12 01:52:29 +0900

    Create 1931.py

---
+++ 1931.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+
+def solve():
+    N = int(input())
+    dic = {}
+    for _ in range(N):
+        start, end = map(int, input().split())
+        if dic.get(start):
+            dic[start].append(end)  # min(dic.get(start, float('inf')), end)
+        else:
+            dic[start] = [end]
+    
+    for k in dic.keys():
+        dic[k].sort()
+        
+    keys = sorted(dic.keys())
+    end = 0
+    count = 0
+    for key in keys:
+        for e in dic[key]:
+            if e < end:
+                end = e
+            elif key >= end:
+                count += 1
+                end = e
+
+    print(count)
+
+
+solve()
