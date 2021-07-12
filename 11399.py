301291dffc52359ae920166f2d80f3f7853ada25
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-07-13 05:45:38 +0900

    Create 11399.py

---
+++ 11399.py
@@ -0,0 +1,13 @@
+import sys
+
+number_of_people = int(sys.stdin.readline())
+waiting_time = list(map(int, sys.stdin.readline().split()))
+waiting_time = sorted(waiting_time, key=lambda x: x)
+
+total_time = 0
+accumulated_time = 0
+for i in range(len(waiting_time)):
+    accumulated_time += waiting_time[i]
+    total_time += accumulated_time
+
+print(total_time)
