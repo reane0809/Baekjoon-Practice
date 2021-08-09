231917d88f5cbf52e6744e768d39525ccd3b59b5
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-08-10 00:51:58 +0900

    Create 11050.cpp

---
+++ 11050.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+//반복문 사용
+int Facto(int num){
+    if(num == 0) 
+        return 1;
+    
+    int result = 1;
+    
+    for(int i=num; i>=1 ; i--){
+        result *= i;
+    }
+
+    return result;
+}
+ 
+ 
+int main(void){
+    
+    int n, k;
+    cin >> n >> k;
+    cout << Facto(n) / (Facto(k) * Facto(n-k));
+    
+    return 0;    
+}
