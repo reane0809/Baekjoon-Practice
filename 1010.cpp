0a4da03988a16a1b8f8cf09b4f05ec95744af2bb
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-08-15 01:54:33 +0900

    Update 1010.cpp

--- 1010.cpp
+++ 1010.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int t;
+int a, b;
+int check[31][31];
+int comb(int n, int m) {
+	if (n == m || m == 0) {
+		return 1;
+	}
+	else if (m < 0 || n < m) {
+		return 0;
+	}
+	else {
+		if (check[n][m])return check[n][m];
+		else {
+			check[n][m] = comb(n - 1, m - 1) + comb(n - 1, m);
+			return check[n][m];
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
+	cin >> t;
+	while (t--) {
+		cin >> a >> b;
+		cout << comb(b, a) << "\n";
+	}
+}
