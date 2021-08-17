3a562caf68067f2b898982d2b07dbe02228a925d
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-08-18 00:57:41 +0900

    Create 1676.cpp

---
+++ 1676.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int two, five, N, i;
+    two = 0;
+    five = 0;
+
+    scanf("%d", &N);
+    for (i = 2; i <= N; i *= 2) {
+        two += N/i;
+    }
+
+        for (i = 5; i <= N; i *= 5) {
+        five += N/i;
+    }
+
+    two < five ? printf("%d\n", two) : printf("%d\n", five);
+    return 0;
+}
