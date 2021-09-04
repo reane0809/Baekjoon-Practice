19c822f977a5fc7922e01324e86d30979425a89c
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-09-05 00:25:03 +0900

    Create 1874.py

---
+++ 1874.py
@@ -0,0 +1,26 @@
+import sys
+In = sys.stdin.readline
+Out = sys.stdout.write
+
+def produce(seq):
+    cur = 1
+    stack = [0]
+    result = ''
+    for x in seq:
+        while cur <= x:
+            stack.append(cur)
+            result += '+\n'
+            cur += 1
+        if stack.pop() != x:
+            Out("NO\n")
+            return
+        else:
+            result += '-\n'
+    Out(result)
+
+def main():
+    n = int(In())
+    seq = [int(In()) for x in range(n)]
+    produce(seq)
+
+main()
