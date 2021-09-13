8a2ae95fc44d6994af997f84d1565461c1ce62eb
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-09-14 01:12:00 +0900

    Create 2164.py

---
+++ 2164.py
@@ -0,0 +1,13 @@
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n = int(input())
+    if(n==2 or n==1):
+        print(n)
+    else:
+        x = 0
+        while 2**x < n:
+            x += 1
+        y = n - 2**(x-1)
+        print(2*y)
