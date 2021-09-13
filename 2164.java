5114f1d748417e3c9a6db2e15ce8d17e029550d8
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-09-14 01:11:47 +0900

    Create 2164.java

---
+++ 2164.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int powOf2 = 1;
+        while (powOf2 < N) {
+            powOf2 = powOf2 << 1;
+        }
+        if (N < powOf2) {
+            powOf2 = powOf2 >> 1;
+        }
+
+        int answer = N == powOf2 ? N : 2 * (N-powOf2);
+        System.out.println(answer);
+        br.close();
+    }
+}
