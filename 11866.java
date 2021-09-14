39a54504c440e5dc663865d61479e5f8ee5b1433
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-09-15 00:32:38 +0900

    Create 11866.java

---
+++ 11866.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> list = new ArrayList<>();
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s);
+		int n = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+        
+		StringBuilder sb = new StringBuilder("<");
+		for(int i=1; i<=n; i++) 
+			list.add(i);
+		
+		int i = b - 1;
+		while(true) {		
+			sb.append(list.get(i));
+			list.remove(i);
+			
+			if(list.size() == 0) 
+				break;
+			else 
+				sb.append(", ");
+			i += (b-1);
+
+			if(i >= list.size()) 
+				i = (i % list.size());			
+		}
+		sb.append(">");
+
+		System.out.println(sb);
+	}
+}
