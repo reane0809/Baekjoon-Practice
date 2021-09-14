a3c952cb76cae04489e7dd1c799265cb0ea25bc5
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-09-15 00:32:05 +0900

    Create 11866.java

---
+++ 11866.java
@@ -0,0 +1,9 @@
+n,k = map(int,input().split())
+li=list(range(1,n+1))
+i=0;s=[]
+while (len(li)!=0):
+    i+=k-1
+    if len(li)<=i:
+        i=i%len(li)
+    s.append(li.pop(i))
+print('<'+str(s)[1:-1]+'>')
