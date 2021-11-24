3892259bb32ae19ae5fcd96ce5e229f458078afc
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-11-25 01:53:30 +0900

    Create 1654.java

---
+++ 1654.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int K = in.nextInt();
+		int N = in.nextInt();
+ 
+		int[] arr = new int[K];
+		
+		long max = 0;
+		
+		// 입력과 동시에 해당 랜선의 길이가 최댓값인지 확인 후 max값 갱신
+		for (int i = 0; i < K; i++) {
+			arr[i] = in.nextInt();
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		
+ 
+		// max에서 +1값
+		max++; 
+ 
+		long min = 0; // 탐색 길이 최솟값
+		long mid = 0; 
+ 
+		while (min < max) { 
+			
+			// 범위 내 중간 길이 도출
+			mid = (max + min) / 2;
+			
+ 
+			long count = 0;
+			
+			// 구해진 중간 길이로 잘라서 생성 갯수 확인
+ 
+			for (int i = 0; i < arr.length; i++) {
+				count += (arr[i] / mid);
+			}
+			
+			/*
+			 *  mid길이로 잘랐을 때의 개수가 만들고자 하는 랜선의 개수보다 작다면
+			 *  자르고자 하는 길이를 줄이기 위해 최대 길이를 줄인다.
+			 *  그 외에는 자르고자 하는 길이를 늘려야 하므로 최소 길이를 늘린다.
+			 */
+			if(count < N) {
+				max = mid;
+			}
+			else {
+				min = mid + 1;
+			}
+			
+ 
+		}
+		
+		// UpperBound로 얻어진 값(min)에 -1이 최대 길이가 된다.
+		System.out.println(min - 1);
+	}
+}
