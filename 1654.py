e9da0fbb99c36dba4b4e68af2c2ea5c5917a5473
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-11-26 01:33:43 +0900

    Create 1654.py

---
+++ 1654.py
@@ -0,0 +1,11 @@
+from sys import stdin
+k, n = map(int, input().split())
+l = [int(x) for x in stdin.read().splitlines()]
+f = lambda x: sum([y//x for y in l])
+bgn, end, mid = 1, max(l)+1, max(l)//2+1
+while end>bgn+1:
+    if f(mid)>=n:
+        bgn, mid = mid, (mid+end)//2
+    else:
+        mid, end = (bgn+mid)//2, mid
+print(bgn)
