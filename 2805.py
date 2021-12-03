4fc8bbbaac92e32267a0ae82c571e47712b61c98
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-12-04 01:41:19 +0900

    Create 2805.py

---
+++ 2805.py
@@ -0,0 +1,21 @@
+import sys
+from collections import Counter
+
+
+def jwy():
+    n, m = map(int, sys.stdin.readline().split())
+    woods = Counter(map(int, sys.stdin.read().split())).items()
+    s, e = 0, max(woods)[0]
+    answer = 0
+    while s <= e:
+        mid = (s + e) // 2
+        if sum([(wood - mid) * c if wood > mid else 0 for wood, c in woods]) >= m:
+            answer = mid
+            s = mid + 1
+        else:
+            e = mid - 1
+    print(answer)
+   
+
+if __name__ == '__main__':
+    jwy()
