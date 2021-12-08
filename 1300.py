e6170e5af32b3a61f57ffbade75205dbf2c576e6
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2021-12-09 00:51:40 +0900

    Create 1300.py

---
+++ 1300.py
@@ -0,0 +1,23 @@
+n = int(input())
+k = int(input())
+
+start = 1
+end = n * n
+result = 0
+
+while start <= end :
+    mid = (start + end) // 2
+    
+    cnt = 0
+    # 각 행에서 mid보다 작거나 같은 수의 개수를 센다.
+    for i in range(1,n+1) :
+        cnt += min( mid // i, n)
+    
+    # mid보다 작거나 같은 수의 개수가 k가 작으면
+    if cnt < k :
+        start = mid + 1
+    else : 
+        end = mid - 1
+        result = mid
+        
+print(result)
