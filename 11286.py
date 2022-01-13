0d841fc955f024c5ff1ce11ffa6e51074631f370
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2022-01-14 00:13:23 +0900

    Create 11286.py

---
+++ 11286.py
@@ -0,0 +1,28 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+
+plus = []
+minus = []
+
+for _ in range(n):
+    x = int(input())
+    if x > 0:
+        heapq.heappush(plus, x)
+    elif x < 0:
+        heapq.heappush(minus, abs(x))
+    else:
+        if plus and minus:
+            if plus[0] < minus[0]:
+                print(heapq.heappop(plus))
+            else:
+                print(-heapq.heappop(minus))
+        elif plus and not minus:
+            print(heapq.heappop(plus))
+        elif not plus and minus:
+            print(-heapq.heappop(minus))
+        else:
+            print(0)
