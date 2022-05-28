8e52d88d695d4b62a4940f674c02968d59cb55aa
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2022-05-29 01:28:19 +0900

    Create 14425.java

---
+++ 14425.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	//문자열 집합
+	static int N;
+	static int M;
+	static HashMap<String, Boolean> data = new HashMap<String, Boolean>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int result=0;
+		for(int i=0; i<N; i++) {
+			data.put(br.readLine(), true);
+		}
+		
+		for(int i=0; i<M; i++) {
+			if(data.get(br.readLine()) != null)
+				result++;
+		} 
+		
+		System.out.println(result);
+	}
+}
