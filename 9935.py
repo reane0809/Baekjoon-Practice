7296aa59bd0fa169780514a8cb73ef5199c2f633
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2022-10-06 00:46:52 +0900

    Create 9935.py

---
+++ 9935.py
@@ -0,0 +1,87 @@
+import os
+import sys
+
+DEBUG = False
+
+
+def main(f=None):
+    init(f)
+    # ######## INPUT AREA BEGIN ##########
+
+    s = input().strip()
+    b = input().strip()
+    b_l = list(b)
+    ans = []
+
+    for i in s:
+        ans.append(i)
+        if i == b[-1] and ans[-len(b):] == b_l:
+            for _ in range(len(b)):
+                ans.pop()
+        
+    print(''.join(ans) if ans else 'FRULA')
+
+    # ######## INPUT AREA END ############
+
+
+
+enu = enumerate
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+def pr(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
