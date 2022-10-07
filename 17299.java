40fa8a9ff05043ac4116b1f9699db4afec401c65
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2022-10-08 01:47:18 +0900

    Create 17299.java

---
+++ 17299.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int[] countSort = new int[1_000_000 + 1];
+    static int[] A, NGF;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        read();
+        dp();
+        for (int i : NGF) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    private static void dp() {
+        NGF[n - 1] = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (countSort[A[i]] < countSort[A[i + 1]]) {
+                NGF[i] = i + 1;
+            } else if (countSort[A[i]] == countSort[A[i + 1]]) {
+                NGF[i] = NGF[i + 1];
+            } else{
+                int next = NGF[i + 1];
+                while (next != -1){
+                    if (countSort[A[i]] < countSort[A[next]]) {
+                        NGF[i] = next;
+                        break;
+                    } else if (countSort[A[i]] == countSort[A[next]]) {
+                        NGF[i] = NGF[next];
+                        break;
+                    }
+                    next = NGF[next];
+                }
+                if(NGF[i] == 0) NGF[i] = -1;
+            }
+        }
+
+        for (int i = 0; i < NGF.length; i++) {
+            if(NGF[i] != -1) {
+                NGF[i] = A[NGF[i]];
+            }
+        }
+    }
+
+    private static void read() throws IOException {
+        n = Integer.parseInt(br.readLine());
+        A = new int[n];
+        NGF = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            countSort[num]++;
+            A[i] = num;
+        }
+    }
+
+    private static void printArr() {
+        for (int i = 0; i < A.length; i++) {
+            System.out.printf("%d ", A[i]);
+        }
+        System.out.println(
+
+        );
+
+        for (int i = 0; i < A.length; i++) {
+            System.out.printf("%d ", countSort[A[i]]);
+        }
+    }
+}
